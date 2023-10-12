8f6169190e1bb11b854ff37bd43261947e3cefea
yarazarin <89267356+yarazarin@users.noreply.github.com>
2023-10-12 16:26:15 -0400

    fixed

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,5 +1,5 @@
 // user-login-form.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -7,37 +7,39 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+  styleUrls: ['./user-login-form.component.scss'],
 })
 export class UserLoginFormComponent implements OnInit {
-
-  credentials = { Username: '', Password: '' };
+  @Input() userData = { Username: '', Password: '' };
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login(): void {
-    this.fetchApiData.userLogin(this.credentials).subscribe(
+    this.fetchApiData.userLogin(this.userData).subscribe(
       (response) => {
         localStorage.setItem('token', response.token);
         localStorage.setItem('user', response.user.Username);
-        this.dialogRef.close(); // Close the login dialog on success.
+        this.dialogRef.close();
         console.log(response);
         this.snackBar.open('Login successful', 'OK', {
-          duration: 2000
+          duration: 2000,
         });
       },
       (error) => {
         console.log(error);
-        this.snackBar.open('Login failed. Please check your credentials and try again.', 'OK', {
-          duration: 2000
-        });
+        this.snackBar.open(
+          'Login failed. Please check your credentials and try again.',
+          'OK',
+          {
+            duration: 2000,
+          }
+        );
       }
     );
   }
