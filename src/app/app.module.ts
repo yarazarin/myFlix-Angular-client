c95022d126e8027e6907690d5883e36152a5a365
yara <uraeel@gmail.com>
2023-10-19 17:37:06 -0400

    .03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { MatListModule } from '@angular/material/list';
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+  { path: '', redirectTo: 'welcome!', pathMatch: 'prefix' },
 ];
 
 
